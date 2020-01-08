35a05ada221a7c6b2692321646c1bcd51b6f0c7f
explorati <w1748419447@163.com>
2020-01-08 22:47:37 +0800

    测试类基类

--- src/test/java/com/explorati/o2o/BaseTest.java
+++ src/test/java/com/explorati/o2o/BaseTest.java
@@ -12,6 +12,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 //告诉junit spring配置文件的位置
-@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+@ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
 public class BaseTest {
 }
