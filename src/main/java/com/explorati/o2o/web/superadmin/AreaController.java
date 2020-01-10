470f00c1005d2d921b3b79aa0512dc2de6e2fa1f
explorati <w1748419447@163.com>
2020-01-10 13:47:31 +0800

    区域Controller

--- src/main/java/com/explorati/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/explorati/o2o/web/superadmin/AreaController.java
@@ -3,6 +3,9 @@ package com.explorati.o2o.web.superadmin;
 import com.explorati.o2o.VO.AreaVO;
 import com.explorati.o2o.entity.Area;
 import com.explorati.o2o.service.AreaService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.mysql.cj.conf.PropertyKey.logger;
+
 /**
  * @ Author : Weijian_Wang
  * @ Date : Created in 15:55 2020/1/7 0007
@@ -21,13 +26,19 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/superadmin")
+@Slf4j
 public class AreaController {
+//    Logger logger = LoggerFactory.getLogger(AreaController.class);
+
     @Autowired
     private AreaService areaService;
 
     @RequestMapping(value = "listarea", method = RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> listArea() {
+        //使用Lombok的Slf4J
+        log.info("===start===");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap = new HashMap<>();
         List<Area> list = new ArrayList<>();
 //        AreaVO areaVO = new AreaVO();
@@ -45,6 +56,11 @@ public class AreaController {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+        log.error("test error!");
+        long endTime = System.currentTimeMillis();
+        //debug信息一般用来调优
+        log.debug("costTime: [{}ms]", endTime - startTime);
+        log.info("===end===");
         return modelMap;
     }
 }
