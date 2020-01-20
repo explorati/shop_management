923e49bad15d62d549196d8379d6791d675124e8
explorati <w1748419447@163.com>
2020-01-20 21:48:39 +0800

    商铺Dao

--- src/main/java/com/explorati/o2o/dao/ShopDao.java
+++ src/main/java/com/explorati/o2o/dao/ShopDao.java
@@ -1,6 +1,9 @@
 package com.explorati.o2o.dao;
 
 import com.explorati.o2o.entity.Shop;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * @ Author : Weijian_Wang
@@ -8,8 +11,29 @@ import com.explorati.o2o.entity.Shop;
  * @ Description ：商铺DAO
  */
 public interface ShopDao {
+
+    /**
+     * 分页查询店铺，可输入的条件有：店铺名(模糊)、店铺状态、店铺类别、区域ID，owner
+     *
+     * @param shopCondition
+     * @param rowIndex      从第几行开始取数据
+     * @param pageSize      返回的条数
+     * @return
+     */
+    List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex, @Param(
+            "pageSize") int pageSize);
+
+    /**
+     * 根据shopId查询店铺
+     *
+     * @param shopId
+     * @return
+     */
+    Shop queryByShopId(long shopId);
+
     /**
      * 新增店铺
+     *
      * @param shop
      * @return
      */
@@ -17,6 +41,7 @@ public interface ShopDao {
 
     /**
      * 更新店铺
+     *
      * @param shop
      * @return
      */
